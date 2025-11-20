1631dbd088b4f185086e19514215da0565b63f00
SteamedHams3 <146746723+SteamedHams3@users.noreply.github.com>
2025-11-20 11:13:13 +0000

    toggle styled and functional

--- highcontrast.js
+++ highcontrast.js
@@ -1,17 +1,23 @@
 const themeSwitch = document.getElementById('switch');
+const switchText = document.getElementById('switch-text');
 
 if (localStorage.getItem('high-contrast-mode') === 'active') {
     document.body.classList.add('high-contrast-mode');
     themeSwitch.checked = true;
+    switchText.textContent = "Disable High Contrast Mode"
+} else{
+    switchText.textContent = "Enable High Contrast Mode"
 }
 const enableHighContrastMode = () => {
     document.body.classList.add('high-contrast-mode');
     localStorage.setItem('high-contrast-mode', 'active');
-}
+    switchText.textContent = "Disable High Contrast Mode";
+} 
 
 const disableHighContrastMode = () => {
     document.body.classList.remove('high-contrast-mode');
     localStorage.setItem('high-contrast-mode', null);
+    switchText.textContent = "Enable High Contrast Mode";
 }
 
 themeSwitch.addEventListener("change", () => {
